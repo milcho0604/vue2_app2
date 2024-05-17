d3118ac53e83c0de0e3291550c90f2305568a758
milcho0604 <milcho0604@gmail.com>
2024-05-17 16:26:07 +0900

    checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
+  <div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+      <MyCheckbox3 />
+    </div>
+  </template>
+  
+  <script>
+  import MyCheckbox3 from './MyCheckbox3.vue'
+  
+  export default {
+    name: 'App',
+    components: {
+      MyCheckbox3
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
+  <style scoped>
+  div { vertical-align: top; margin: 10px; }
+  img { margin: 10px; }
+  </style>
+  
